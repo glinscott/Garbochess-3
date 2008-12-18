d1ba14d9e46a9360fad40b946b5414856577aed1
unknown <Gary@.(none)>
2008-12-18 02:13:35 -0800

    Update make.bat to generate PDB's for profiling.

--- GarboChess3/x64/make.bat
+++ GarboChess3/x64/make.bat
@@ -1,5 +1,5 @@
-cl /Ox /Oi /Ot /favor:INTEL64 /EHs-c- /GL /MT /GS- /GR- /W3 /nologo /D "X64" /D "NDEBUG" /TP ../main.cpp ../movegen.cpp ../position.cpp ../search.cpp ../psqTables.cpp ../windows.cpp ../evaluation.cpp ../tests.cpp ../utilities.cpp /link /OUT:"GarboChess3.exe" /INCREMENTAL:NO
-copy /Y garbochess3.exe ..
-pushd ..
-garbochess3.exe
-popd
+cl /Zi /Ox /Oi /Ot /favor:INTEL64 /EHs-c- /GL /MT /GS- /GR- /W3 /nologo /D "X64" /D "NDEBUG" /TP ../main.cpp ../movegen.cpp ../position.cpp ../search.cpp ../psqTables.cpp ../windows.cpp ../evaluation.cpp ../tests.cpp ../utilities.cpp /link /OUT:"GarboChess3.exe" /INCREMENTAL:NO /DEBUG
+REM copy /Y garbochess3.exe ..
+REM pushd ..
+REM garbochess3.exe
+REM popd
